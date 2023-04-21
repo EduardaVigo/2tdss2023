d357c170a7bb44aaf809f57edc60d222e6d21e50
eduarda.vigo <rm93596@fiap.com.br>
2023-04-20 22:33:11 -0300

    Aprendendo ImageBackground

--- FiapApp/App.js
+++ FiapApp/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
+        <ImageBackground
+        source={require('./assets/fiap_logo.jpg')}
+        resizeMode = 'contain'
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto"/>
+      </ImageBackground>
       </View>
     );  
   }
